edef5918e92f841cb2c27c348203b3e5089f5ada
mikeriss <mypic@mail.com>
2023-04-27 17:49:18 +0200

    - github login works correctly

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import LogInButton from "../Components/LogInButton"
+
+function HomePage() {
+    return(
+        <>
+        <LogInButton />
+        </>
+
+
+    ) 
+  }
+  
+  export default HomePage
